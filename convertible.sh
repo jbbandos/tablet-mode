4904f1ef57f6c544bdfad9d37a3e836036216552
Bernardo Bandos <jbbandos@users.noreply.github.com>
2025-01-19 08:12:57 +0100

    Update convertible.sh
    Configuration options better identified and at the start of the script.
    Event ID obtained from event name.
    Input IDs no longer used, just names.
    Names should be more stable than IDs, hopefully.

--- convertible.sh
+++ convertible.sh
@@ -12,6 +12,46 @@
 # user needs to be in the input group:
 #    sudo gpasswd --add $username input
 # then logout and login again
+# ----------------------------------------------------------------------------------
+# configuration
+# ----------------------------------------------------------------------------------
+# edit the following for your specific system events and devices
+# use "libinput list-devices" to identify the touchscreen device, keyboard, etc
+# "xinput --list" does not list switches
+
+tScreen="Elan Touchscreen"
+tPad="Elan Touchpad"
+modeSwitch="Tablet Mode Switch"
+
+# if you're using the keyd keymap as suggested from 
+# https://docs.chrultrabook.com/docs/installing/distros.html
+# the keyboard is "keyd", if not it should be "AT Translated"
+# might be different on your setup
+#kBoard="AT Translated Set 2 keyboard"
+kBoard="keyd virtual keyboard" 
+
+# This query should return your lcd screen - verify in your terminal
+sName=$(xrandr |grep "\sconnected"|cut -d" " -f1 | head -1)
+# sName="eDP" # If the query doesn't work for you, just hardcode the lcd screen name
+
+# use "libinput list-kernel-devices" to identify the tablet mode switch. 
+# if you get "permission denied" errors, you forgot to add your
+# user to the input group
+# The output usually will include lines like this:
+# /dev/input/event0:	Lid Switch
+# /dev/input/event1:	Power Button
+# /dev/input/event2:	AT Translated Set 2 keyboard
+# /dev/input/event3:	Video Bus
+# /dev/input/event4:	Tablet Mode Switch
+# /dev/input/event5:	Elan Touchscreen
+# /dev/input/event6:	Elan Touchpad
+
+tabModeSwitch=$(libinput list-kernel-devices |grep "$modeSwitch"|cut -d":" -f1)
+# tabModeSwitch="/dev/input/event4"
+
+# ----------------------------------------------------------------------------------
+# configuration end
+# ----------------------------------------------------------------------------------
 
 
 # use flock to ensure a single instance is running
@@ -45,44 +85,6 @@ echo $$ > "$LOCKFILE"
 
 trap mortalWound EXIT  # Ensure the lock file is deleted on exit
 
-# edit the following for your specific system events and devices
-# use "libinput list-devices" to identify the touchscreen device, keyboard, etc
-# "xinput --list" does not list switches
-touchScreen="Elan Touchscreen"
-touchPad="Elan Touchpad"
-modeSwitch="Tablet Mode Switch"
-keyBoard="AT Translated Set 2 keyboard"
-vkeyBoard="keyd virtual keyboard"
-# This query should return your lcd screen - verify in your terminal
-sName=$(xrandr |grep "\sconnected"|cut -d" " -f1 | head -1)
-# sName="eDP" # If the query doesn't work for you, just hardcode the lcd screen name
-
-
-# use "xinput list" to obtain id and master id for keyboard and touchpad
-# master ids are only needed if using float/reattach instead of enable/disable
-# eg - 
-# Elan Touchpad                           	id=9	[slave  pointer  (2)]
-#
-tpadID=9
-#tpadMaster=2
-#
-# AT Translated Set 2 keyboard            	id=11	[slave  keyboard (3)]
-# keyd virtual keyboard                   	id=12	[slave  keyboard (3)]
-# if you're using the keyd keymap as suggested from 
-# https://docs.chrultrabook.com/docs/installing/distros.html
-# the keyboard is "keyd", if not it is the "AT Translated"
-#
-kbdID=11
-#kbdMaster=3
-
-
-# use "libinput --list-devices" to identify the tablet mode switch. 
-# if you get "permission denied" errors, you forgot to add your
-# user to the input group
-# The output usually will include like this:
-# Device:           Tablet Mode Switch
-# Kernel:           /dev/input/event4
-tabModeSwitch="/dev/input/event4"
 
 # this function assumes you use Onboard as on-screen keyboard,
 # and uses dbus to enable/disable its auto-show
@@ -92,18 +94,19 @@ mode_switch() {
         if [ "$mode" = "1" ]; then
             #	echo "tablet"
             # disable keyboard
-            xinput disable $kbdID
+            xinput disable "$kBoard"
             # disable touchpad
-            xinput disable $tpadID
-	        dbus-send --type=method_call --print-reply --dest=org.onboard.Onboard /org/onboard/Onboard/Keyboard org.freedesktop.DBus.Properties.Set string:"org.onboard.Onboard.Keyboard" string:"AutoShowPaused" variant:boolean:"false" &>/dev/null
+            xinput disable "$tPad"
+	    # tell onboard to show up automatically
+	    dbus-send --type=method_call --print-reply --dest=org.onboard.Onboard /org/onboard/Onboard/Keyboard org.freedesktop.DBus.Properties.Set string:"org.onboard.Onboard.Keyboard" string:"AutoShowPaused" variant:boolean:"false" &>/dev/null
         else 
             # 	echo "laptop"
             # enable keyboard
-            xinput enable $kbdID
+            xinput enable "$kBoard"
             # enable touchpad
-            xinput enable $tpadID
+            xinput enable "$tPad"
 	    #pause onboard auto-show.
-	        dbus-send --type=method_call --print-reply --dest=org.onboard.Onboard /org/onboard/Onboard/Keyboard org.freedesktop.DBus.Properties.Set string:"org.onboard.Onboard.Keyboard" string:"AutoShowPaused" variant:boolean:"true" &>/dev/null
+     	    dbus-send --type=method_call --print-reply --dest=org.onboard.Onboard /org/onboard/Onboard/Keyboard org.freedesktop.DBus.Properties.Set string:"org.onboard.Onboard.Keyboard" string:"AutoShowPaused" variant:boolean:"true" &>/dev/null
     fi
     done
 }
@@ -116,20 +119,20 @@ auto_rotate() {
         case "$ORIENTATION" in
 
                 normal)
-                xrandr --output $sName --rotate normal
-                xinput set-prop "$touchScreen" "Coordinate Transformation Matrix" 1 0 0 0 1 0 0 0 1
+                xrandr --output "$sName" --rotate normal
+                xinput set-prop "$tScreen" "Coordinate Transformation Matrix" 1 0 0 0 1 0 0 0 1
                 ;;
             left-up)
-                xrandr --output $sName --rotate left
-                xinput set-prop "$touchScreen" "Coordinate Transformation Matrix" 0 -1 1 1 0 0 0 0 1
+                xrandr --output "$sName" --rotate left
+                xinput set-prop "$tScreen" "Coordinate Transformation Matrix" 0 -1 1 1 0 0 0 0 1
                 ;;
             bottom-up)
-                xrandr --output $sName --rotate inverted
-                xinput set-prop "$touchScreen" "Coordinate Transformation Matrix" -1 0 1 0 -1 1 0 0 1
+                xrandr --output "$sName" --rotate inverted
+                xinput set-prop "$tScreen" "Coordinate Transformation Matrix" -1 0 1 0 -1 1 0 0 1
                 ;;
             right-up)
-                xrandr --output $sName --rotate right
-                xinput set-prop "$touchScreen" "Coordinate Transformation Matrix" 0 1 0 -1 0 1 0 0 1
+                xrandr --output "$sName" --rotate right
+                xinput set-prop "$tScreen" "Coordinate Transformation Matrix" 0 1 0 -1 0 1 0 0 1
                 ;;
         esac
     done
